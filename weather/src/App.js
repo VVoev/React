5c8be85a57ab586da8f5e22e82c3cd9f57d3560b
VVoev <vladovoev@yahoo.co.uk>
2017-08-04 18:02:20 +0300

    api is giving some response

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import Towns from './Components/Towns'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h2>Welcome to Weather App React</h2>
+                <h3>Please enter a town you wish to see weather</h3>
+                <Towns/>
             </div>
         );
     }
---
+++ weather/src/Components/Towns.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import WeatherRequester from './Requester/WeatherRequester'
+import $ from 'jquery'
+
+export default class Towns extends Component {
+    render() {
+        return (
+            <div className="container">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="usr">Town:</label>
+                        <input type="text" className="form-control" id="town"></input>
+                        <button type="button" className="btn btn-success" onClick={this.submitClicked.bind(this)}>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    submitClicked() {
+        let town = $('#town').val();
+        WeatherRequester.getTownInfoByTown(town)
+            .then((x)=>{
+            console.log(x)
+            })
+    }
+
+
+}
