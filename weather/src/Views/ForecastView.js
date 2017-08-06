5284d02edc3c4d9f494cb36133792b1c399802d7
VVoev <vladovoev@yahoo.co.uk>
2017-08-06 13:31:26 +0300

    added some bootstrap

--- weather/src/Views/ForecastView.js
+++ weather/src/Views/ForecastView.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import Bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+const kelvin = 273.15;
 
 export default class ForecastView extends Component {
     render() {
         let name = this.props.name;
-        const kelvin = 273.15;
         let weatherRows = this.props.fullInfo.list.map(info =>
            <tr>{name}
                 <td>{parseFloat(info.main.temp_min-kelvin).toFixed(2)}</td>
@@ -14,13 +15,13 @@ export default class ForecastView extends Component {
         return (
             <div className="weather-view">
                 <h1>Weather Forecast</h1>
-                <table className="weather-table">
+                <table className="col-xs-6 col-xs-offset-3">
                     <thead>
                     <tr>
-                        <th>Town</th>
-                        <th>Min Temperature</th>
-                        <th>Max Temperature</th>
-                        <th>Time</th>
+                        <th className="text-center">Municipality[Town]</th>
+                        <th className="text-center">Min Temperature</th>
+                        <th className="text-center">Max Temperature</th>
+                        <th className="text-center">Time</th>
                     </tr>
                     </thead>
                     <tbody>
